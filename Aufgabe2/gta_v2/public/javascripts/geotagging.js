9882cefbf71e03fda1a0036a16b0b68c4d1641db
Lena Reiser <lena-reiser@t-online.de>
2022-11-21 11:34:55 +0100

    ok

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -103,7 +103,9 @@ class MapManager {
  * It is called once the page has been fully loaded.
  */
 // ... your code here ...
-
+function updateLocation{
+    findLocation;
+}
 // Wait for the page to fully load its DOM content, then call updateLocation
 document.addEventListener("DOMContentLoaded", () => {
     alert("Please change the script 'geotagging.js'");
