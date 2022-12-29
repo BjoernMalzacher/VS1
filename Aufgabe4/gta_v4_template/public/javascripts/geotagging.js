136a6f77c28e78f36ad35c514d2870b3dfb821eb
Lena Reiser <lena-reiser@t-online.de>
2022-12-29 12:10:38 +0100

    fchbruiubibvuiö

---
+++ Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
@@ -0,0 +1,56 @@
+// File origin: VS1LAB A2
+
+/* eslint-disable no-unused-vars */
+// This script is executed when the browser loads index.htm
+// "console.log" writes to the browser's console. 
+// The console window must be opened explicitly in the browser.
+// Try to find this output in the browser...
+console.log("The geoTagging script is going to start...");
+
+/**
+ * TODO: 'updateLocation'
+ * A function to retrieve the current location and update the page.
+ * It is called once the page has been fully loaded.
+ */
+function updateLocation(){
+    try {
+        lat = document.getElementById("latHidden")?.getAttribute("value");
+        long = document.getElementById("Longhidden")?.getAttribute("value");
+        if(!lat && !long){
+            LocationHelper.findLocation(overwriteLocation); 
+       
+        }else{
+            createMap(lat, long);
+   
+        } 
+    } catch (error) {
+        alert(error.message);    
+    } 
+}
+function createMap(lat, long){
+    mManager = new MapManager("GNQ8FCI311cYDR2EQ9UtoCZGidMfvBIK");
+    let json_list =  document.getElementById("mapView").getAttribute("data-tags");
+   //let json_list= $("#mapView").data("tags"); 
+   if(!json_list){
+    json_list =[];
+   }
+   console.log( json_list+"++++++   ");
+    mpQuestURL = mManager.getMapUrl(lat, long);
+    document.getElementById("mapView")?.setAttribute("src", mpQuestURL);
+}
+function overwriteLocation(helper){
+        document.getElementById("Latitude_value")?.setAttribute("value",helper.latitude);
+        document.getElementById("Longitude_value")?.setAttribute("value",helper.longitude);
+        document.getElementById("latHidden")?.setAttribute("value", helper.latitude);
+        document.getElementById("longHidden")?.setAttribute("Value", helper.longitude);
+        createMap(helper.latitude, helper.longitude);
+    }  
+   
+
+
+
+// Wait for the page to fully load its DOM content, then call updateLocation
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("i am getting called");
+    updateLocation();       
+});
--- Aufgabe4/gta_v4_template/routes/index.js
+++ Aufgabe4/gta_v4_template/routes/index.js
@@ -9,7 +9,7 @@
 /**
  * Define module dependencies.
  */
-
+const parser = require('body-parser');
 const express = require('express');
 const router = express.Router();
 
@@ -19,7 +19,8 @@ const router = express.Router();
  */
 // eslint-disable-next-line no-unused-vars
 const GeoTag = require('../models/geotag');
-
+router.use(parser.urlencoded({extended: true }));
+router.use(express.json());
 /**
  * The module "geotag-store" exports a class GeoTagStore. 
  * It provides an in-memory store for geotag objects.
@@ -38,8 +39,73 @@ const GeoTagStore = require('../models/geotag-store');
  * As response, the ejs-template is rendered without geotag objects.
  */
 
+
 router.get('/', (req, res) => {
-  res.render('index', { taglist: [] })
+  res.render('index', { 
+    taglist: [],
+    taglist_json: "",
+    latitude : "", 
+    longitude : ""});
+});
+
+/**
+ * Route '/tagging' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests cary the fields of the tagging form in the body.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * Based on the form data, a new geotag is created and stored.
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the new geotag.
+ * To this end, "GeoTagStore" provides a method to search geotags 
+ * by radius around a given location.
+ */
+router.post('/tagging', (req, res) => {
+  let name = req.body.name;
+  let lat = req.body.latitude;
+  let long = req.body.longitude;
+  let hash = req.body.hashtag;  
+  geotag = new GeoTag(name, lat, long ,hash);
+  tagStore.addGeoTag(geotag);
+  let neartags = tagStore.getNearbyGeoTags(geotag, 1);
+  res.render("index", {
+    taglist: neartags,
+    latitude: lat,
+    longitude: long,
+    taglist_json: JSON.stringify(neartags)
+  });
+});
+/**
+ * Route '/discovery' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests cary the fields of the discovery form in the body.
+ * This includes coordinates and an optional search term.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the given coordinates.
+ * If a search term is given, the results are further filtered to contain 
+ * the term as a part of their names or hashtags. 
+ * To this end, "GeoTagStore" provides methods to search geotags 
+ * by radius and keyword.
+ */
+
+router.post('/discovery', (req, res) => {  
+  let searchvalue = req.body.Search;
+  let lat = req.body.lat_hidden; 
+  let long = req.body.long_hidden;
+
+  geotag = new GeoTag("none", lat, long, "none");
+  list = tagStore.searchNearbyGeoTags(geotag, 1,searchvalue);
+  res.render("index", {
+    taglist: list ,
+    latitude: lat,
+    longitude:long,
+    taglist_json: JSON.stringify(list)
+  });
 });
 
 // API routes (A4)
